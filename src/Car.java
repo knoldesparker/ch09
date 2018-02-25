f7de758897ff4383305a6425d7752d51667d7737
knoldesparker <5735792+knoldesparker@users.noreply.github.com>
2018-02-25 11:17:20 +0100

    done 4

--- src/Car.java
+++ src/Car.java
@@ -8,12 +8,32 @@ public class Car {
 
     @Override
     public String toString() {
-        return "vroom";
+        return "vroom";         //Makes a toString
     }
 }
 class Truck extends Car {
-    public void m1() {
+    public void m1() { //Overrides the m1 from car
         System.out.println("truck 1");
     }
 
+}
+class SportUtilityVehicle extends Car{
+    public static void main(String[] args) {
+        SportUtilityVehicle SUV = new SportUtilityVehicle();
+
+        SUV.m1();
+        SUV.m2();
+        System.out.println(SUV);
+    }
+    public void m1(){ //Overrides the m1 method again
+        System.out.println("SUV 1");
+    }
+    public void m2(){
+        super.m1();     //super calls the original m1 method from car
+    }
+
+    @Override
+    public String toString() {
+        return "SUV " + super.toString(); //combines the super toString from car with SUV
+    }
 }
